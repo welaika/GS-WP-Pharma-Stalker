7e1258e323be245ebdfd91502e0c6b51e20aadbc
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-11-07 18:06:29 +0100

    query improvements

--- lib/lib.database.sh
+++ lib/lib.database.sh
@@ -4,7 +4,7 @@ function search_db {
 	local database=$1
 	local prefix=$2
 
-	mysql_invoke $database "SELECT option_name, option_value FROM ${prefix}options WHERE ( option_name LIKE 'rss_%' OR option_name LIKE '%doorgen%' ) AND ( option_value LIKE '%base%' OR option_value LIKE '%esab%' OR option_value LIKE '%==%' OR option_value REGEXP '[0-9a-zA-Z\+]{100,}') \G" | grep 'option_value'
+	mysql_invoke $database "SELECT option_name, option_value FROM ${prefix}options WHERE ( option_name LIKE 'rss_%' OR option_name LIKE '%doorgen%' ) AND ( option_value LIKE '%base%' OR option_value LIKE '%esab%' OR option_value LIKE '%==%' OR option_value REGEXP '[0-9a-zA-Z\+]{100,}') \G" | grep 'option_name'
 }
 
 function mysql_invoke {
