91096b0934598558999f0443e6a9d81cd059c2c5
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-11-07 17:35:40 +0100

    fixed bug in sql query construction

--- lib/lib.database.sh
+++ lib/lib.database.sh
@@ -4,7 +4,7 @@ function search_db {
 	local database=$1
 	local prefix=$2
 
-	mysql_invoke $database "SELECT n, v FROM (SELECT option_name as n, option_value as v FROM wp_options) AS opt WHERE ( n LIKE 'rss_%' OR n LIKE '%doorgen%' ) AND ( v LIKE '%base%' OR v LIKE '%esab%' OR v LIKE '%==%' OR v REGEXP '[0-9a-zA-Z\+]{100,}') \G"
+	mysql_invoke $database "SELECT n, v FROM (SELECT option_name as n, option_value as v FROM ${prefix}wp_options) AS opt WHERE ( n LIKE 'rss_%' OR n LIKE '%doorgen%' ) AND ( v LIKE '%base%' OR v LIKE '%esab%' OR v LIKE '%==%' OR v REGEXP '[0-9a-zA-Z\+]{100,}') \G"
 }
 
 function mysql_invoke {
